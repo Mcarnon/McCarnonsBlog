edd8549e9749592fceb57d0b66b0143690b469d7
Mcarnon <3273556124@qq.com>
2026-08-04 15:32:12 +0800

    fix mobile navbar

--- theme/components/navbar/LmNav.vue
+++ theme/components/navbar/LmNav.vue
@@ -22,42 +22,48 @@ const homeItem = computed(() => themeConfig.value.navbar.find(item => item.link 
     class="lm-nav flex flex-col w-full transition-transform duration-250 ease-in-out relative"
   >
     <div class="px-4 py-2 flex gap-3 w-full items-center sm:px-5">
-      <RouterLink
-        to="/"
-        class="lm-nav-home hidden md:inline-flex"
-      >
-        <span
-          v-if="homeItem?.icon"
-          class="lm-nav-home__icon"
-          :class="homeItem.icon"
-        />
-        <span>{{ homeItem?.text }}</span>
-      </RouterLink>
+      <span class="hidden md:contents">
+        <RouterLink
+          to="/"
+          class="lm-nav-home"
+        >
+          <span
+            v-if="homeItem?.icon"
+            class="lm-nav-home__icon"
+            :class="homeItem.icon"
+          />
+          <span>{{ homeItem?.text }}</span>
+        </RouterLink>
+      </span>
 
-      <LmNavBrand class="md:hidden" />
+      <span class="md:hidden">
+        <LmNavBrand />
+      </span>
 
-      <button
-        type="button"
-        class="lm-nav-desktop-hamburger hidden md:inline-flex"
-        :aria-expanded="props.desktopDrawerOpen"
-        aria-label="Toggle navigation menu"
-        @click="emit('toggleDesktopDrawer')"
-      >
-        <span class="lm-nav-desktop-hamburger-lines">
-          <span
-            class="lm-nav-desktop-hamburger-line"
-            :class="[props.desktopDrawerOpen ? 'translate-y-[5px] rotate-45 w-4' : 'w-4']"
-          />
-          <span
-            class="lm-nav-desktop-hamburger-line"
-            :class="[props.desktopDrawerOpen ? 'opacity-0' : 'w-4']"
-          />
-          <span
-            class="lm-nav-desktop-hamburger-line"
-            :class="[props.desktopDrawerOpen ? '-translate-y-[5px] -rotate-45 w-4' : 'w-4']"
-          />
-        </span>
-      </button>
+      <span class="hidden md:contents">
+        <button
+          type="button"
+          class="lm-nav-desktop-hamburger"
+          :aria-expanded="props.desktopDrawerOpen"
+          aria-label="Toggle navigation menu"
+          @click="emit('toggleDesktopDrawer')"
+        >
+          <span class="lm-nav-desktop-hamburger-lines">
+            <span
+              class="lm-nav-desktop-hamburger-line"
+              :class="[props.desktopDrawerOpen ? 'translate-y-[5px] rotate-45 w-4' : 'w-4']"
+            />
+            <span
+              class="lm-nav-desktop-hamburger-line"
+              :class="[props.desktopDrawerOpen ? 'opacity-0' : 'w-4']"
+            />
+            <span
+              class="lm-nav-desktop-hamburger-line"
+              :class="[props.desktopDrawerOpen ? '-translate-y-[5px] -rotate-45 w-4' : 'w-4']"
+            />
+          </span>
+        </button>
+      </span>
 
       <LmNavTools
         :drawer-open="props.drawerOpen"
